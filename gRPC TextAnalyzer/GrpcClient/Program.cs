576f11d2619eb2f05683bf0c25b79623da7d6da7
aashishpatil117 <aapatil@barracuda.com>
2025-02-24 10:09:27 +0530

    Add files via upload

---
+++ gRPC TextAnalyzer/GrpcClient/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using Grpc.Net.Client;
+using System.Threading.Tasks;
+using Grpc.Core;
+
+namespace GrpcClient{
+    class Program{
+        static async Task Main(string[] args){
+            var channel=GrpcChannel.ForAddress("http://localhost:5106");
+            var textclient=new TextAnalysis.TextAnalysisClient(channel);
+            var result=await textclient.TextAnalyzeAsync(new TextRequest(){
+                Name="!!!!"
+            });
+            Console.WriteLine($"word count={result.Wc}\nChar count={result.Cc}\nis palindrome={result.IsP}");
+            //Console.WriteLine("snake");
+        }
+    }
+}
+
